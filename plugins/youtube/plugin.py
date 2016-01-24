f98b712f35b24eb5645d0f4834b0c1bdeaea363d
John Maguire <contact@johnmaguire.me>
2016-01-23 23:49:20 -0500

    Update youtube plugin with decorators

--- plugins/youtube/plugin.py
+++ plugins/youtube/plugin.py
@@ -4,10 +4,15 @@ import urllib
 import urllib2
 import logging
 
+from cardinal.decorators import command, event, help
 from cardinal.exceptions import EventRejectedMessage
 
-VIDEO_URL_REGEX = re.compile(r'https?:\/\/(?:www\.)?youtube\..{2,4}\/watch\?.*(?:v=(.+?))(?:(?:&.*)|$)', flags=re.IGNORECASE)
-VIDEO_URL_SHORT_REGEX = re.compile(r'https?:\/\/(?:www\.)?youtu\.be\/(.+?)(?:(?:\?.*)|$)', flags=re.IGNORECASE)
+VIDEO_URL_REGEX = re.compile(
+    r'https?:\/\/(?:www\.)?youtube\..{2,4}\/watch\?.*(?:v=(.+?))(?:(?:&.*)|$)',
+    flags=re.IGNORECASE)
+VIDEO_URL_SHORT_REGEX = re.compile(
+    r'https?:\/\/(?:www\.)?youtu\.be\/(.+?)(?:(?:\?.*)|$)',
+    flags=re.IGNORECASE)
 
 
 class YouTubePlugin(object):
@@ -27,37 +32,40 @@ class YouTubePlugin(object):
         if 'api_key' in config:
             self.api_key = config['api_key']
 
-        self.callback_id = cardinal.event_manager.register_callback(
-            'urls.detection', self._get_video_info
-        )
-
+    @command('youtube', 'yt')
+    @help("Get the first YouTube result for a given search.",
+          "Syntax: .youtube <search query>")
     def search(self, cardinal, user, channel, msg):
         # Before we do anything, let's make sure we'll be able to query YouTube
         if self.api_key is None:
-            cardinal.sendMsg(channel, "YouTube plugin is not configured correctly. Please set API key.")
+            return cardinal.sendMsg(
+                channel,
+                "YouTube plugin is not configured correctly."
+                "Please set API key.")
 
         # Grab the search query
         try:
             search_query = msg.split(' ', 1)[1]
         except IndexError:
-            cardinal.sendMsg(channel, "Syntax: .youtube <search query>")
-            return
+            return cardinal.sendMsg(channel, "Syntax: .youtube <search query>")
 
-        params = {'q': search_query, 'part': 'snippet', 'maxResults': 1, 'type': 'video'}
+        params = {'q': search_query, 'part': 'snippet', 'maxResults': 1,
+                  'type': 'video'}
 
         try:
             result = self._form_request("search", params)
-        except Exception, e:
+        except Exception:
             cardinal.sendMsg(channel, "Unable to connect to YouTube.")
             self.logger.exception("Failed to connect to YouTube")
             return
 
         if 'error' in result:
-            cardinal.sendMsg(channel, "An error occurred while attempting to search YouTube.")
             self.logger.error(
-                "Error attempting to search YouTube: %s" % content['error']
+                "Error attempting to search YouTube: %s" % result['error']
             )
-            return
+            return cardinal.sendMsg(
+                channel,
+                "An error occurred while attempting to search YouTube.")
 
         try:
             video_id = str(result['items'][0]['id']['videoId'].encode('utf-8'))
@@ -73,9 +81,9 @@ class YouTubePlugin(object):
 
         try:
             result = self._form_request("videos", params)
-        except Exception, e:
-            cardinal.sendMsg(channel, "Unable to connect to YouTube.")
+        except Exception:
             self.logger.exception("Failed to connect to YouTube")
+            cardinal.sendMsg(channel, "Unable to connect to YouTube.")
             return
 
         try:
@@ -83,14 +91,11 @@ class YouTubePlugin(object):
             cardinal.sendMsg(channel, message)
         except IndexError:
             cardinal.sendMsg(channel, "No videos found matching that search.")
-        except Exception, e:
+        except Exception:
             self.logger.exception("Failed to parse info for %s'" % video_id)
             raise EventRejectedMessage
 
-    search.commands = ['youtube', 'yt']
-    search.help = ["Get the first YouTube result for a given search.",
-                   "Syntax: .youtube <search query>"]
-
+    @event('urls.detection')
     def _get_video_info(self, cardinal, channel, url):
         match = re.match(VIDEO_URL_REGEX, url)
         if not match:
@@ -99,18 +104,19 @@ class YouTubePlugin(object):
             raise EventRejectedMessage
 
         video_id = match.group(1)
-        params = {'id': video_id, 'maxResults': 1, 'part': 'snippet,statistics'}
+        params = {'id': video_id, 'maxResults': 1,
+                  'part': 'snippet,statistics'}
 
         try:
             result = self._form_request("videos", params)
-        except Exception, e:
+        except Exception:
             self.logger.exception("Failed to fetch info for %s'" % video_id)
             raise EventRejectedMessage
 
         try:
             message = self._parse_item(result['items'][0])
             cardinal.sendMsg(channel, message)
-        except Exception, e:
+        except Exception:
             self.logger.exception("Failed to parse info for %s'" % video_id)
             raise EventRejectedMessage
 
@@ -120,8 +126,8 @@ class YouTubePlugin(object):
 
         # Make request to specified endpoint and return JSON decoded result
         uh = urllib2.urlopen("https://www.googleapis.com/youtube/v3/" +
-            endpoint + "?" +
-            urllib.urlencode(params))
+                             endpoint + "?" +
+                             urllib.urlencode(params))
 
         return json.load(uh)
 
@@ -136,11 +142,10 @@ class YouTubePlugin(object):
         # (Hint: If this breaks, that's probably why.)
         video_id = str(item['id'].encode('utf-8'))
 
-        return ("[ Title: %s | Uploaded by: %s | %s views | https://www.youtube.com/watch?v=%s ]" %
+        return ("[ Title: %s | Uploaded by: %s | %s views | "
+                "https://www.youtube.com/watch?v=%s ]" %
                 (title, uploader, "{:,}".format(views), video_id))
 
-    def close(self, cardinal):
-        cardinal.event_manager.remove_callback('urls.detection', self.callback_id)
 
 def setup(cardinal, config):
     return YouTubePlugin(cardinal, config)
