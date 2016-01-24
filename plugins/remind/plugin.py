caaa0113102d80b0b403c6c0d56f966b949b0691
John Maguire <contact@johnmaguire.me>
2016-01-23 23:20:33 -0500

    Update remind plugin with user tuple

--- plugins/remind/plugin.py
+++ plugins/remind/plugin.py
@@ -5,21 +5,22 @@ from cardinal.decorators import command, help
 
 class RemindPlugin(object):
     @command('remind')
-    @help("Sends a reminder after a set time.")
-    @help("Syntax: .remind <minutes> <message>")
+    @help("Sends a reminder after a set time.",
+          "Syntax: .remind <minutes> <message>")
     def remind(self, cardinal, user, channel, msg):
+        nick, _, _ = user
         message = msg.split(None, 2)
         if len(message) < 3:
             cardinal.sendMsg(channel, "Syntax: .remind <minutes> <message>")
             return
 
         timer = threading.Timer(60 * int(message[1]),
-                                cardinal.sendMsg, (user.group(1), message[2]))
+                                cardinal.sendMsg, (nick, message[2]))
         timer.start()
 
         cardinal.sendMsg(channel,
                          "%s: You will be reminded in %d minutes." %
-                         (user.group(1), int(message[1])))
+                         (nick, int(message[1])))
 
 
 def setup():
