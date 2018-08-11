a85c1688e914593adf3cd34f7c5a430e3fee3c0f
John Maguire <jmaguire@duosecurity.com>
2018-08-11 17:08:25 -0400

    Fix lint errors in movie plugin

--- plugins/movie/plugin.py
+++ plugins/movie/plugin.py
@@ -1,14 +1,15 @@
-import logging, requests
+import logging
+
+import requests
+
+from cardinal.decorators import command, help
+
 
 class MoviePlugin(object):
-    logger = None
-    """Logging object for MoviePlugin."""
-
-    api_key = None
-    """Api key for omdb api."""
-
     def __init__(self, cardinal, config):
         self.logger = logging.getLogger(__name__)
+
+        self.api_key = None
 
         if config is None:
             return
@@ -16,10 +17,14 @@ class MoviePlugin(object):
         if "api_key" in config:
             self.api_key = config['api_key']
 
+    @command(['movie', 'omdb', 'imdb'])
+    @help('Get the first OMDb result for a given search.')
+    @help('Syntax: .movie <search_query>')
     def search(self, cardinal, user, channel, msg):
-        # Before we do anything, let's make sure we'll be able to query omdb.
+        # Before we do anything, let's make sure we'll be able to query OMDb.
         if self.api_key is None:
-            cardinal.sendMsg(channel, "Movie plugin is not configured correctly. Please set API key.")
+            cardinal.sendMsg(channel, "Movie plugin misconfigured. "
+                                      "Please set api_key.")
             return
 
         try:
@@ -32,19 +37,19 @@ class MoviePlugin(object):
 
         try:
             result = self._form_request(params)
-        except Exception, e:
-            cardinal.sendMsg(channel, "Unable to connect to omdb.")
-            self.logger.exception("Failed to connect to omdb")
+        except Exception:
+            cardinal.sendMsg(channel, "Unable to connect to OMDb.")
+            self.logger.exception("Failed to connect to OMDb")
             return
 
         if result['Response'] == 'False':
             if "Error" in result:
                 cardinal.sendMsg(channel, bytes(result['Error']))
+                self.logger.error("Error attempting to search OMDb: {}".format(
+                                    result['Error']))
             else:
-                cardinal.sendMsg(channel, "An error occurred while attempting to search omdb.")
-                self.logger.error(
-                    "Error attempting to search omdb: %s" % content['Error']
-                )
+                cardinal.sendMsg(channel, "An error occurred while attempting "
+                                          "to search OMDb.")
                 return
 
         try:
@@ -55,43 +60,40 @@ class MoviePlugin(object):
             }
         # We should never reach this but just in case..
         except IndexError:
-            cardinal.sendMsg(channel, "Unable to get movie id.")
+            cardinal.sendMsg(channel, "Unable to get movie ID.")
             return
 
         try:
             result = self._form_request(params)
-        except Exception, e:
-            cardinal.sendMsg(channel, "Unable to connect to omdb.")
-            self.logger.exception("Failed to connect to omdb.")
+        except Exception:
+            cardinal.sendMsg(channel, "Unable to connect to OMDb.")
+            self.logger.exception("Failed to connect to OMDb.")
             return
 
         try:
             message = self._parse_data(result)
             cardinal.sendMsg(channel, message)
-        except Exception, e:
+        except Exception:
+            cardinal.sendMsg(channel, "Failed to prase info for %s" % movie_id)
             self.logger.exception("Failed to parse info for %s" % movie_id)
-            raise EventRejectedMessage
-
-    search.commands = ['movie', 'omdb', 'imdb']
-    search.help = ['Get the first imdb result for a given search.',
-                   'Syntax: .movie <search_query>']
 
     def _form_request(self, payload):
         payload['apikey'] = self.api_key
 
-        return requests.get('http://www.omdbapi.com', params = payload).json()
+        return requests.get('http://www.omdbapi.com', params=payload).json()
 
     def _parse_data(self, data):
-        title  = str(data['Title'].encode('utf-8'))
-        year   = str(data['Year'].encode('utf-8'))
+        title = str(data['Title'].encode('utf-8'))
+        year = str(data['Year'].encode('utf-8'))
         rating = str(data['imdbRating'].encode('utf-8'))
-        genre  = str(data['Genre'].encode('utf-8'))
-    #   plot   = str(data['Plot'].encode('utf-8'))
+        genre = str(data['Genre'].encode('utf-8'))
 
         movie_id = str(data['imdbID'].encode('utf-8'))
 
-        return ("[ Title: %s | Year: %s | Rating: %s | Genre: %s | http://imdb.com/title/%s ]" %
-            (title, year, rating, genre, movie_id))
+        return ("[ Title: %s | Year: %s | Rating: %s | Genre: %s | "
+                "http://imdb.com/title/%s ]" % (
+                    title, year, rating, genre, movie_id))
+
 
 def setup(cardinal, config):
     return MoviePlugin(cardinal, config)
